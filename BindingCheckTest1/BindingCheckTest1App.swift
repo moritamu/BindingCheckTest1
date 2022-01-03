8e2ee4d300a875f9821f642cc9d5f0c0b4063ed2
moritamu <moris@r.sannet.ne.jp>
2022-01-04 08:28:20 +0900

    Initial Commit

---
+++ BindingCheckTest1/BindingCheckTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  BindingCheckTest1App.swift
+//  BindingCheckTest1
+//
+//  Created by 森田晋 on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct BindingCheckTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
